3689675597bddf9a1651ca3f62ff0efd24f2444e
Samuel OLOFU <coolsamuel71@gmail.com>
2023-07-22 10:01:31 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ __printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - Function to print values according to specifiers
+ * @format: the format received
+ * Return: The number of character printed excluding the null byte
+ */
+int _printf(const char *format, ...)
+{
+
+}
