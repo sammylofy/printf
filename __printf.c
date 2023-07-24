d3fd691e952b553a94c8ba43888a69f9cf3dba8c
Samuel OLOFU <coolsamuel71@gmail.com>
2023-07-24 11:59:12 -0700

    updated printf

--- __printf.c
+++ __printf.c
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 
 	if (format == NULL)
-		return (0);
+		return (-1);
 
 	while (format[i] != '\0')
 	{
@@ -89,7 +89,7 @@ static int printString(char *str)
 
 	if (str == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 	else
 	{
