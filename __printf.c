ddcb80b67b58521dfe473c4ede0e49d33773664e
Samuel OLOFU <coolsamuel71@gmail.com>
2023-07-25 14:54:17 -0700

    added binary function

--- __printf.c
+++ __printf.c
@@ -3,6 +3,7 @@
 static int printChar(char item);
 static int printString(char *str);
 static int printIntValue(int value);
+int printBinary(unsigned int num);
 
 /**
  * _printf - a custom implementation of printf
@@ -46,17 +47,17 @@ int _printf(const char *format, ...)
 
 				noPrintedChar += printString(str);
 			}
-			else if (s == 'd')
+			else if (s == 'd' || s == 'i')
 			{
 				int num = va_arg(args, int);
 
 				noPrintedChar += printIntValue(num);
 			}
-			else if (s == 'i')
+			else if (s == 'b')
 			{
-				int val = va_arg(args, int);
+				unsigned int val = va_arg(args, unsigned int);
 
-				noPrintedChar += printIntValue(val);
+				noPrintedChar += printBinary(val);
 			}
 			else if (s == '%')
 			{
@@ -128,6 +129,7 @@ static int printIntValue(int value)
 	{
 		printChar('-');
 		value = -value;
+		digits++;
 	}
 	if (value < 10)
 	{
@@ -136,5 +138,24 @@ static int printIntValue(int value)
 	}
 	digits = printIntValue(value / 10);
 	printChar('0' + value % 10);
-	return (digits + 1);
+	return (digits);
+}
+/**
+ * printBinary - Function that handles binary conversion
+ * @num: The integer to be printed
+ * Return: The number of digits printed
+ */
+
+int printBinary(unsigned int num)
+{
+	int count = 0;
+
+	if (num == 0)
+	{
+		return (1);
+	}
+	count = printBinary(num / 2);
+	printChar('0' + num % 2);
+
+	return (count);
 }
